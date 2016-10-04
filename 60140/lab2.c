2905eac6490ba810e6df138d9e4aa71ff117e01d
Mohamed Murtadi <murtadim@cs.uwindsor.ca>
2016-10-04 11:02:10 -0400

    lab 2 part 2 complete

--- 60140/lab2.c
+++ 60140/lab2.c
@@ -6,28 +6,29 @@
 int main(void)
 
 {
-	// Part A
-	int num1 = 40000;
-	float num2 = 1.234;
-
-	int mm, dd, yyyy;
-
-	printf("%-15.8d\n",num1); 00040000
+	// Part A Print unsigned integer 40000 left justified in a 15-digit field with 8 digits.
+	
+    int num1 = 40000;
+    
+    printf("%-15.8d\n",num1);
+    
+    
+    
+    // Part B Print 1.234 in a 9-digit field with preceding zeros.
+	
+    float num2 = 1.234;
 
 	printf("%09f\n", num2); 
 	
-	printf("please enter the month you were born. (1-12): ");
-	scanf("%d", &mm);
-	
-	printf("please enter the day you were born. (1-31): ");
-	scanf("%d", &dd);
-	
-	printf("please enter the year you were born. (1900-2015): ");
-	scanf("%d", &yyyy);
-	
+    
+    // Part C Read a time of the form hh:mm:ss, storing the parts of the time in the integer variables hour, minute and second. Skip the colons (:) in the input stream. 
+    
+	int hh, mm, ss;
 
-	scanf("%d:%d:%d", &mm,&dd &yyyy);
-	
+    printf("time in the format HH:MM:SS with semicolons included: ");
+	scanf("%d:%d:%d", &hh, &mm, &ss);
+
+    printf("hh is: %02d \nmm is: %02d \nss is: %02d\n", hh, mm, ss);
 
 	return 0;
 }
