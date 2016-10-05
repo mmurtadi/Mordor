d0766090bc36e42b7d70b7ad059d236ab9767819
Mohamed Murtadi <murtadim@uwindsor.ca>
2016-10-05 12:59:25 -0400

    lab 3 start

---
+++ 60140/lab 3.c
@@ -0,0 +1,19 @@
+// Name: Mohamed Murtadi
+// Purpose: CS 60-140 Fall2016
+// lab 3
+#include <stdio.h>
+
+int main(void)
+
+{
+    int group;
+    int publisher;
+    int title;
+    int c_digit;
+
+
+    printf("Enter the ISBN Number:");
+	scanf("", );
+
+	return 0;
+}
