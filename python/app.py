b2262129777e6255e2fdbb939bbe4d19b97918b7
mmurtadi <mmurtadi@gmail.com>
2016-10-14 01:05:53 -0400

    pycharm installed and beginning to use as main IDE

---
+++ python/app.py
@@ -0,0 +1,7 @@
+student = {"name": "Moe",
+           "mark": [70, 50, 80, 44, 99],
+           "exams": {
+                "final": [70],
+                "midterm": [88]
+            }}
+print(student['exams']['final'])
