624142a00c83d9b2b8690d0917d9299d653b6ca6
Moe Murtadi <murtadim@uwindsor.ca>
2016-10-20 14:21:36 -0400

    Lab3 raptor comments cleaned up

--- 60140/lab4/exprEval.c
+++ 60140/lab4/exprEval.c
@@ -16,17 +16,22 @@ int main(void)
 	scanf("%f%c%f", &num1, &op, &num2);
 	
 
-	if (num2 == 0) 
-		printf("0 is undefined, please try again\n");
-	else 
-		if (op == '+')
-			result = num1+num2;
-		else if (op == '/')
+ 
+	if (op == '+') {
+		result = num1+num2;
+		printf("Answer!!! pew pew... Daily Double!...\njk, answer is %.2f\n", result);
+	}
+	else if (op == '/') {
+		if (num2 == 0) 
+			printf("0 is undefined, please try again\n");
+		else
 			result = num1/num2;
-		else if (op == '*'){
-			result = num1*num2;
 			printf("Answer!!! pew pew... Daily Double!...\njk, answer is %.2f\n", result);
-		}
+	}
+	else if (op == '*'){
+		result = num1*num2;
+		printf("Answer!!! pew pew... Daily Double!...\njk, answer is %.2f\n", result);
+	}
 
 	return 0;
 }
