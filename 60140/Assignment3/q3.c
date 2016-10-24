a51540139bda8f5076ef82674ad02e5cd935f135
Moe Murtadi <murtadim@uwindsor.ca>
2016-10-24 19:11:35 -0400

    lab4 complete

---
+++ 60140/Assignment3/q3.c
@@ -0,0 +1,16 @@
+// Name: Mohamed Murtadi
+// Purpose: CS 60-140 Fall2016
+// Assignment 3 Question 2
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <=128) {
+		printf("%d",i);
+		i *=2;
+	}
+	return 0;
+}
