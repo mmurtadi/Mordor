66fd201977bf8b9d775e64c7debb52afd4c91128
mmurtadi <mmurtadi@gmail.com>
2016-11-14 01:52:00 -0500

    gtphotobooth website touch ups

--- mmurtadi 2.0/mail/contact_me.php
+++ mmurtadi 2.0/mail/contact_me.php
@@ -16,10 +16,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'murtadim@uwindsor.ca'; /
+$to = 'mmurtadi@gmail.com'; /
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@mmurtadi.com\n"; 
+$headers = "From: admin@mmurtadi.com\n"; 
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
